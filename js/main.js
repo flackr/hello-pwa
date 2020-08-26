19dd1f32471b8e9b9abece4dc83c160fcd10f206
Robert Flack <flackr@gmail.com>
2020-08-26 12:13:26 -0400

    Log keypresses.

--- js/main.js
+++ js/main.js
@@ -6,3 +6,14 @@ window.onload = () => {
              .register('./sw.js');
   }
 }
+
+document.body.addEventListener('keyup', log);
+document.body.addEventListener('keydown', log);
+document.body.addEventListener('keypress', log);
+
+function log(evt) {
+  let log = document.getElementById('log');
+  log.textContent = evt.type + ' (keycode ' + evt.keyCode + ')\n' + log.textContent;
+  evt.preventDefault();
+  evt.stopPropagation();
+}
